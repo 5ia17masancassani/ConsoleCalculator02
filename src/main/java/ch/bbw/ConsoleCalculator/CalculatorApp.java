08277e6b823e8a23711d0c72a4fab94c48090875
5ia17masancassani <5ia17masancassani@gibwintra.ch>
2018-12-10 08:51:53 +0100

    Console Ausgabe erweitert

--- src/main/java/ch/bbw/ConsoleCalculator/CalculatorApp.java
+++ src/main/java/ch/bbw/ConsoleCalculator/CalculatorApp.java
@@ -19,6 +19,9 @@ public class CalculatorApp {
 		val1 = 10;
 		val2 = 20;
 		System.out.println("Summe " + val1 + " + " + val2 + " = " + calc.addition(10, 20));
+		System.out.println("Differenz " + val2 + " - " + val1 + " = " + calc.subtraktion(20, 10));
+		System.out.println("Produkt " + val1 + " * " + val2 + " = " + calc.multiplikation(10, 20));
+		System.out.println("Quotient " + val2 + " / " + val1 + " = " + calc.division(20, 10));
 		
 		
 	}
